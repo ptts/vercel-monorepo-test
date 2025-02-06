72b7af0e462e22dff7d821d454261c5904cd83ac
ptts <patrick.steffen@filu.vet>
2025-02-06 21:15:27 +0100

    feat: enhance API handler to support additional HTTP methods

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,9 +1,28 @@
-import { app } from "../src/index.js";
 import { handle } from "hono/vercel";
 
-export const config = {
+import { app } from "../src/index.js";
+
+/**
+ * Vercel Function Entrypoint
+ * @see https://github.com/vercel/hono-starter/blob/main/api/index.js
+ */
+
+/**
+ * @see https://vercel.com/docs/functions/configuring-functions/runtime#edge
+ * To run with "nodejs" runtime, set `config.runtime` to `undefined`
+ */
+export const config: { runtime: "edge" | undefined } = {
   runtime: "edge",
 };
 
-export const GET = handle(app);
-export const POST = handle(app);
+const handler = handle(app);
+
+export const GET = handler;
+export const PUT = handler;
+export const POST = handler;
+export const DELETE = handler;
+export const PATCH = handler;
+export const HEAD = handler;
+export const OPTIONS = handler;
+export const TRACE = handler;
+export const CONNECT = handler;
